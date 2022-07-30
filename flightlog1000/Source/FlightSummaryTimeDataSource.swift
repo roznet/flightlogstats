cdc886993e7cebe58be9ef308a683152877a5d26
brice <briceguard-git@yahoo.com>
2022-07-30 14:42:16 +0100

    Summary time table upgraded to TableDataSource

--- flightlog1000/Source/FlightSummaryTimeDataSource.swift
+++ flightlog1000/Source/FlightSummaryTimeDataSource.swift
@@ -8,21 +8,15 @@
 import UIKit
 import OSLog
 
-class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, TableCollectionDelegate {
+class FlightSummaryTimeDataSource: TableDataSource {
     let flightSummary : FlightSummary
     let displayContext : DisplayContext
-    
-    private var attributedCells : [NSAttributedString] = []
-
-    var frozenColumns : Int = 1
-    var frozenRows : Int = 1
-    
-    private(set) var sections : Int = 0
-    private(set) var items : Int = 6
-    
+        
     init(flightSummary : FlightSummary, displayContext : DisplayContext = DisplayContext()){
         self.flightSummary = flightSummary
         self.displayContext = displayContext
+        
+        super.init(rows: 0, columns: 6, frozenColumns: 1, frozenRows: 1)
         
     }
     
@@ -38,31 +32,33 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
     ///   - since: this will display elapsed time in the range in time format (should be start = turn on, end beginning of event/section to report)
     ///   - elapsed: this will display elapsed time in the range in decimal format (should be start of period (start of moving, start of flight, etc) to end of period (moving, flight, etc)
     func prepareRow(title : String, description : String, since : TimeRange? = nil, elapsed : TimeRange? = nil){
-        self.attributedCells.append(NSAttributedString(string: title, attributes: self.titleAttributes))
-        self.attributedCells.append(NSAttributedString(string: description, attributes: self.cellAttributes))
+        self.cellHolders.append(CellHolder(string: title, attributes: self.titleAttributes))
+        self.cellHolders.append(CellHolder(string: description, attributes: self.cellAttributes))
         
         if let date = since?.end {
-            self.attributedCells.append(NSAttributedString(string: self.displayContext.format(time: date),
+            self.cellHolders.append(CellHolder(string: self.displayContext.format(time: date),
                                                            attributes: self.cellAttributes))
         }else{
-            self.attributedCells.append(NSAttributedString(string: "", attributes: self.cellAttributes))
+            self.cellHolders.append(CellHolder(string: "", attributes: self.cellAttributes))
         }
         
         if let since = since, since.elapsed > 0 {
-            self.attributedCells.append(NSAttributedString(string: self.displayContext.formatHHMM(timeRange: since),
+            self.cellHolders.append(CellHolder(string: self.displayContext.formatHHMM(timeRange: since),
                                                            attributes: self.cellAttributes))
         }else{
-            self.attributedCells.append(NSAttributedString(string: "", attributes: self.cellAttributes))
+            self.cellHolders.append(CellHolder(string: "", attributes: self.cellAttributes))
         }
         if let elapsed = elapsed {
-            self.attributedCells.append(NSAttributedString(string: self.displayContext.formatHHMM(timeRange: elapsed),
+            self.cellHolders.append(CellHolder(string: self.displayContext.formatHHMM(timeRange: elapsed),
                                                            attributes: self.cellAttributes))
-            self.attributedCells.append(NSAttributedString(string: self.displayContext.formatDecimal(timeRange: elapsed),
+            self.cellHolders.append(CellHolder(string: self.displayContext.formatDecimal(timeRange: elapsed),
                                                            attributes: self.cellAttributes))
         }else{
-            self.attributedCells.append(NSAttributedString(string: "", attributes: self.cellAttributes))
-            self.attributedCells.append(NSAttributedString(string: "", attributes: self.cellAttributes))
+            self.cellHolders.append(CellHolder(string: "", attributes: self.cellAttributes))
+            self.cellHolders.append(CellHolder(string: "", attributes: self.cellAttributes))
         }
+        
+        self.rowsCount += 1
     }
     
     //                 Value    Time   SinceStart  Elapsed   Logbook
@@ -75,15 +71,16 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
     //
     
 
-    func prepare() {
+    override func prepare() {
         
-        self.attributedCells  = []
-        self.sections = 0
+        self.cellHolders  = []
+        self.geometries   = []
+        self.rowsCount = 0
         
         for title in [ "", "", "Time", "Since Start", "Elapsed", "Logbook" ] {
-            self.attributedCells.append(NSAttributedString(string: title, attributes: self.titleAttributes))
+            self.cellHolders.append(CellHolder(string: title, attributes: self.titleAttributes))
         }
-        self.sections += 1
+        self.rowsCount += 1
         
         if let hobbs = self.flightSummary.hobbs,
            let airport = self.flightSummary.startAirport{
@@ -94,7 +91,6 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
         }else{
             self.prepareRow(title: "Start", description: "")
         }
-        self.sections += 1
         
         if  let hobbs = self.flightSummary.hobbs,
             let moving = self.flightSummary.moving {
@@ -103,7 +99,6 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
         }else{
             self.prepareRow(title: "Taxi", description: "")
         }
-        self.sections += 1
         
         if let hobbs = self.flightSummary.hobbs,
             let flying = self.flightSummary.flying {
@@ -115,7 +110,6 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
             self.prepareRow(title: "Takeoff", description: "")
             self.prepareRow(title: "Landing", description: "")
         }
-        self.sections += 2
 
         if  let hobbs = self.flightSummary.hobbs,
             let moving = self.flightSummary.moving {
@@ -124,7 +118,6 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
         }else{
             self.prepareRow(title: "Parked", description: "")
         }
-        self.sections += 1
 
         if let hobbs = self.flightSummary.hobbs {
             var endAirport = ""
@@ -136,46 +129,5 @@ class FlightSummaryTimeDataSource: NSObject, UICollectionViewDataSource, UIColle
         }else{
             self.prepareRow(title: "Shutdown", description: "")
         }
-        self.sections += 1
     }
-    
-    func attributedString(at indexPath : IndexPath) -> NSAttributedString {
-        let index = indexPath.section * self.items + indexPath.item
-        return self.attributedCells[index]
-    }
-    func size(at indexPath: IndexPath) -> CGSize {
-        return self.attributedString(at: indexPath).size()
-    }
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return self.sections
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TableCollectionViewCell", for: indexPath)
-        if let tableCell = cell as? TableCollectionViewCell {
-            tableCell.label.attributedText = self.attributedString(at: indexPath)
-            
-            if indexPath.section < self.frozenRows || indexPath.item < self.frozenColumns{
-                tableCell.backgroundColor = UIColor.systemCyan
-            }else{
-                if indexPath.section % 2 == 0{
-                    tableCell.backgroundColor = UIColor.systemBackground
-                }else{
-                    tableCell.backgroundColor = UIColor.systemGroupedBackground
-                }
-            }
-        }
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        Logger.app.info("Selected \(indexPath)")
-    }
-
 }
