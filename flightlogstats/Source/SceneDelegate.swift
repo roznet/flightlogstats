67bcedc72a383e45a3db380cb3e604dad4e71603
brice <briceguard-git@yahoo.com>
2022-11-05 10:19:25 +0000

    access to log programmatically plus display the function in the log message

--- flightlogstats/Source/SceneDelegate.swift
+++ flightlogstats/Source/SceneDelegate.swift
@@ -52,10 +52,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         Logger.app.info("foreground")
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        
         Logger.app.info("background")
+        
+        
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
