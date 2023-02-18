35b0d9b65ca7ffdd65b933481459299246dcf9dc
brice <briceguard-github@yahoo.com>
2023-02-18 07:16:57 -0700

    fix ci workflow to use proejct instead of workspace

---
+++ flightlogstats/Source/StatsDetailledViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StatsDetailledViewController.swift
+//  FlightLogStats
+//
+//  Created by Brice Rosenzweig on 18/02/2023.
+//
+
+import UIKit
+
+class StatsDetailledViewController: UIViewController {
+
+    @IBOutlet weak var logListCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
