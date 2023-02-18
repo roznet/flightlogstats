52e28d6bc8ee71e7f9d457fcc1908f62cf8230a9
brice <briceguard-github@yahoo.com>
2023-02-18 15:15:26 -0700

    start configure stats for trip

--- flightlogstats/Source/StatsTripsViewController.swift
+++ flightlogstats/Source/StatsTripsViewController.swift
@@ -10,6 +10,8 @@ import OSLog
 
 class StatsTripsViewController: UIViewController {
     @IBOutlet weak var logListCollectionView: UICollectionView!
+    @IBOutlet weak var aggregationSegment: UISegmentedControl!
+    
     var flightListDataSource : FlightListDataSource? = nil
     
     var aggregation : Trips.Aggregation = .trips
@@ -21,7 +23,10 @@ class StatsTripsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        self.updateView()
+        self.rebuildTable()
+    }
+    func rebuildTable() {
         let infos = FlightLogOrganizer.shared.flightLogFileRecords(request: .flightsOnly)
         self.flightListDataSource = FlightListDataSource(logFileRecords: infos, displayContext: DisplayContext(), aggregation: self.aggregation)
         
@@ -32,6 +37,25 @@ class StatsTripsViewController: UIViewController {
         }else{
             Logger.app.error("Internal error: Inconsistent layout ")
         }
-
+        
     }
+    //MARK: - ui controls
+    func updateView() {
+        switch self.aggregation {
+        case .trips:
+            self.aggregationSegment.selectedSegmentIndex = 0
+        case .months:
+            self.aggregationSegment.selectedSegmentIndex = 1
+        }
+    }
+    
+    @IBAction func aggregationChanged(_ sender: Any) {
+        if self.aggregationSegment.selectedSegmentIndex == 0 {
+            self.aggregation = .trips
+        }else if self.aggregationSegment.selectedSegmentIndex == 1 {
+            self.aggregation = .months
+        }
+        self.rebuildTable()
+    }
+    
 }
