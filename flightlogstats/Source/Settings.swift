254ca3b7b5fffa98140ab8429212dc118e4077ab
brice <briceguard-github@yahoo.com>
2023-05-28 16:04:07 +0200

    default should be automatic upload

--- flightlogstats/Source/Settings.swift
+++ flightlogstats/Source/Settings.swift
@@ -126,7 +126,7 @@ struct Settings {
     var savvyToken : String?
    
     
-    @EnumStorage(key: Key.upload_method, defaultValue: .manual)
+    @EnumStorage(key: Key.upload_method, defaultValue: .automatic)
     var uploadMethod : UploadMethod
     
     //Default on macos is selected file, on iOS automatic
